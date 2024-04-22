6a1795265c15f76792366cb9d154fea90bcd1577
agatascalac <agata.kulbicka@scalac.io>
2024-04-22 14:31:38 +0200

    updates

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -30,7 +30,8 @@ export type State = {
   };
   message?: string | null;
 };
-export async function createInvoice(prevState: State, formData: FormData)  {
+
+export async function createInvoice(prevState: State, formData: FormData) {
   const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
